2e560324273ea1fadab416e88fceef29308a0507
DarcProducts <darcproducts@gmail.com>
2023-06-20 10:14:29 -0400

    Data Removal 10/10

--- Assets/Scripts/Interactions/ItemAssets.cs
+++
@@ -1,9 +0,0 @@
-
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ItemAssets", order = 1)]
-public class ItemAssets : ScriptableObject
-{
-    public string[] texts;
-    public AudioClip[] audioClips;
-}
