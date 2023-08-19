01813e197f337862d494bdfb4e1d23bba1a75145
DarcProducts <darcproducts@gmail.com>
2023-08-19 10:36:20 -0400

    Files Moved 3/5

--- Assets/Main_Build/Scripts/HotSpot.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
-
-public sealed class HotSpot : MonoBehaviour
-{
-    [TextArea(0, 10)] public string hotspotInfo;
-    public Vector3 hotspotIconOffset;
-}
--- Assets/Main_Build/Scripts/RotateObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RotateObject : MonoBehaviour
-{
-    [SerializeField] Vector3 rotateVector;
-    [SerializeField] float rotateSpeed;
-
-    void FixedUpdate() => transform.Rotate(rotateSpeed * Time.fixedDeltaTime * rotateVector.normalized, Space.Self);
-}
