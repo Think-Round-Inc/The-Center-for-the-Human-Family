dd8e9d7ce8d3183a349f7b053a939bbefe9e0e0c
choudharyprachi300 <choudharyprachi300@gmail.com>
2024-01-30 18:01:00 -0800

    Spiral tour start

--- Assets/Scripts/Viewer/ViewerController.cs
+++ Assets/Scripts/Viewer/ViewerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public sealed class ViewerController : MonoBehaviour
@@ -10,7 +12,10 @@ public sealed class ViewerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!viewerControllerActive) return;
+        if (!viewerControllerActive)
+        {
+            SpiralTour();
+        }
         RotateViewer();
         MoveCharacter();
     }
@@ -67,7 +72,53 @@ public sealed class ViewerController : MonoBehaviour
 
         transform.Translate(movement, Space.World);
     }
+
+    public List<Transform> targetPositions;
+    public List<Transform> targetRotations;
+    public float moveSpeed = 3f;
+    public float rotSpeed = 3f; 
+    public float pauseDuration = 2f; // Adjust the duration of the pause
+    
+    private float currentTime = 0f;
+    private int currentTargetIndex = 0;
+
+    private void SpiralTour()
+    {
+        // Check if there are target positions and target rotations
+        if (targetPositions.Count == 0 || currentTargetIndex==targetPositions.Count)
+            return;
+
+        // Move towards the current target position
+        transform.position = Vector3.MoveTowards(transform.position, targetPositions[currentTargetIndex].position, moveSpeed * Time.fixedDeltaTime);
+
+        Vector3 directionToFace = targetRotations[currentTargetIndex].position - transform.position;
+
+        if (directionToFace != Vector3.zero)
+        {
+            // Create a rotation based on the direction
+            Quaternion rotation = Quaternion.LookRotation(directionToFace);
+
+            // Smoothly rotate towards the desired direction
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotSpeed * Time.fixedDeltaTime);
+        }
+
+        // Check if the object is close enough to the current target position
+        if (Vector3.Distance(transform.position - new Vector3(0f, 0.985f, 0f), targetPositions[currentTargetIndex].position) <= 0.1f && Quaternion.Angle(transform.rotation, Quaternion.LookRotation(directionToFace)) < 0.1f)
+        {
+            if(targetPositions[currentTargetIndex].CompareTag("PausePoint"))
+            {
+                currentTargetIndex++;
+                currentTime = 0f;
+            }
+            else
+            {
+                currentTime += 0.01f;
+                if (currentTime > pauseDuration)
+                {
+                    currentTargetIndex++;
+                    currentTime = 0f;
+                }
+            }
+        }
+    }
 }
-
-
-
